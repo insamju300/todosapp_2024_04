3c78dfa2869e97f003cb551c0ba425eb1ed1492b
insamju300 <insamju300@gmail.com>
2024-04-01 16:50:17 +0900

    tailwind mui 충돌 대응

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
+'use client';
+import Button from '@mui/material/Button';
+
 export default function Home() {
   return (
     <>
-      <div></div>
+      <div>
+        <Button className="tw-bg-red-500" variant="contained">
+          Contained
+        </Button>
+      </div>
     </>
   );
 }
