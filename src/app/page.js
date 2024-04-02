ac5fd3f468b464b7c6b3cae8c7f8bda6ebef6078
insamju300 <insamju300@gmail.com>
2024-04-02 14:33:57 +0900

    MUI backdrop 컴포넌트

--- src/app/page.js
+++ src/app/page.js
@@ -2,22 +2,43 @@
 
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { Button, Box, AppBar, Toolbar } from '@mui/material';
+import { Button, AppBar, Toolbar, Backdrop, CircularProgress } from '@mui/material';
 import theme from './theme';
-import { MdDeleteForever } from 'react-icons/md';
+import { FaBars } from 'react-icons/fa';
 
 export default function App() {
+  const [open, setOpen] = React.useState(false);
+
   return (
     <>
       <ThemeProvider theme={theme}>
-        <AppBar position="static">
-          <Toolbar className="tw-justify-center">
-            <a className="tw-font-bold" href="/">
-              NOTE!
-            </a>
+        <AppBar position="fixed">
+          <Toolbar>
+            <div className="tw-flex-1">
+              <FaBars className="tw-cursor-pointer" />
+            </div>
+            <div className="logo-box">
+              <a href="/" className="tw-font-bold">
+                NOTE!
+              </a>
+            </div>
+            <div className="tw-flex-1 tw-flex tw-justify-end">
+              <a href="/write">글쓰기</a>
+            </div>
           </Toolbar>
         </AppBar>
+        <Toolbar />
+        <section className="tw-h-screen tw-flex tw-items-center tw-justify-center tw-text-[5rem]">
+          section
+        </section>
       </ThemeProvider>
+      <Button onClick={() => setOpen(true)}>Show backdrop</Button>
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={() => setOpen(false)}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </>
   );
 }
--- src/app/playWithComponent/page.js
+++ src/app/playWithComponent/page.js
@@ -10,8 +10,33 @@ import {
   Button,
   MenuIconButton,
   MenuIcon,
+  Snackbar,
+  Alert,
 } from '@mui/material';
 
 export default function Sub() {
-  return <h1>서브페이지</h1>;
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleClick}>Open Snackbar</Button>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" variant="filled" sx={{ width: '100%' }}>
+          This is a success Alert inside a Snackbar!
+        </Alert>
+      </Snackbar>
+    </div>
+  );
 }
